cbf09fd96bd47f4741fe1b7ce2b7a3849cfe04df
Rifqi Aditya <56026271+rifqiadit10@users.noreply.github.com>
2022-10-27 10:03:22 +0700

    https change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 
 function App() {
  const client = axios.create({
-  baseURL: "http://20.168.8.95:1880/modbus",
+  baseURL: "https://20.168.8.95:1880/modbus",
  });
 
  const [posts, setPosts] = useState({
